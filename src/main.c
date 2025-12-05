fd69466d21985682039720fd3d1a5dd8437b2f55
Nico Carbone <carbone.nicolas0@gmail.com>
2025-12-05 16:20:35 -0500

    sliding attacks done

--- src/main.c
+++ src/main.c
@@ -8,6 +8,17 @@
 
 int main()
 {
+    initSlidingAttacks(); // Initialize magic bitboard tables, must be run at the start
+    CBoard board;
+    const char *fenString = "r1bqkbnr/pppppppp/2n5/8/8/2N5/PPPPPPPP/R1BQKBNR w KQkq - 0 1";
+    board = fenToCBoard((char *)fenString);
+    if (!board.allPieces)
+    {
+        printf("Failed to load FEN: %s\n", fenString);
+        return 1;
+    }
+    printBoard(&board);
 
+    printBBoard(getBishopAttacks(E4, board.allPieces)); // Print bishop attacks from e4 (square 36)
     return 0;
 }
--- src/sliding_attacks.h
+++ src/sliding_attacks.h
@@ -133,184 +133,19 @@ static const Bitboard bishop_occupancy_maps[] = {
     0x0020100804020000ULL,
     0x0040201008040200ULL,
 };
-// the following functions were used to generate the occupancy maps
-// void computeRookOccupancyMaps()
-// {
-//     for (int square = 0; square < 64; square++)
-//     {
-//         int rank = square / 8;
-//         int file = square % 8;
+extern const Bitboard RMagic[64];
+extern const Bitboard BMagic[64];
+extern const int RBits[64];
+extern const int BBits[64];
 
-//         /* squares on same rank, excluding edge files A and H */
-//         Bitboard rankMask = RANKS[rank] & ~(FILE_A | FILE_H);
+Bitboard rook_attacks[64][4096];
+Bitboard bishop_attacks[64][512];
 
-//         /* squares on same file, excluding edge ranks 1 and 8 */
-//         Bitboard fileMask = FILES[file] & ~(RANK_1 | RANK_8);
-
-//         /* combine and remove the current square */
-//         Bitboard mask = (rankMask | fileMask) & ~square_mask(square);
-
-//         printf("0x%016llxULL,\n", mask);
-//     }
-// }
-// void computeBishopOccupancyMaps()
-// {
-//     printf("static const Bitboard bishop_occupancy_maps[] = {\n");
-//     for (int sq = 0; sq < 64; sq++)
-//     {
-//         int rank = sq / 8;
-//         int file = sq % 8;
-//         Bitboard mask = 0ULL;
-
-//         // NE
-//         for (int r = rank + 1, f = file + 1; r <= 6 && f <= 6; r++, f++)
-//             mask |= 1ULL << (r * 8 + f);
-//         // NW
-//         for (int r = rank + 1, f = file - 1; r <= 6 && f >= 1; r++, f--)
-//             mask |= 1ULL << (r * 8 + f);
-//         // SE
-//         for (int r = rank - 1, f = file + 1; r >= 1 && f <= 6; r--, f++)
-//             mask |= 1ULL << (r * 8 + f);
-//         // SW
-//         for (int r = rank - 1, f = file - 1; r >= 1 && f >= 1; r--, f--)
-//             mask |= 1ULL << (r * 8 + f);
-//         printf("    0x%016llxULL,%s", mask, (sq + 1) % 8 == 0 ? "\n" : " ");
-//     }
-//     printf("};\n");
-// }
-const Bitboard RMagic[64] = {
-    0x2080020500400f0ULL,
-    0x28444000400010ULL,
-    0x20000a1004100014ULL,
-    0x20010c090202006ULL,
-    0x8408008200810004ULL,
-    0x1746000808002ULL,
-    0x2200098000808201ULL,
-    0x12c0002080200041ULL,
-    0x104000208e480804ULL,
-    0x8084014008281008ULL,
-    0x4200810910500410ULL,
-    0x100014481c20400cULL,
-    0x4014a4040020808ULL,
-    0x401002001010a4ULL,
-    0x202000500010001ULL,
-    0x8112808005810081ULL,
-    0x40902108802020ULL,
-    0x42002101008101ULL,
-    0x459442200810c202ULL,
-    0x81001103309808ULL,
-    0x8110000080102ULL,
-    0x8812806008080404ULL,
-    0x104020000800101ULL,
-    0x40a1048000028201ULL,
-    0x4100ba0000004081ULL,
-    0x44803a4003400109ULL,
-    0xa010a00000030443ULL,
-    0x91021a000100409ULL,
-    0x4201e8040880a012ULL,
-    0x22a000440201802ULL,
-    0x30890a72000204ULL,
-    0x10411402a0c482ULL,
-    0x40004841102088ULL,
-    0x40230000100040ULL,
-    0x40100010000a0488ULL,
-    0x1410100200050844ULL,
-    0x100090808508411ULL,
-    0x1410040024001142ULL,
-    0x8840018001214002ULL,
-    0x410201000098001ULL,
-    0x8400802120088848ULL,
-    0x2060080000021004ULL,
-    0x82101002000d0022ULL,
-    0x1001101001008241ULL,
-    0x9040411808040102ULL,
-    0x600800480009042ULL,
-    0x1a020000040205ULL,
-    0x4200404040505199ULL,
-    0x2020081040080080ULL,
-    0x40a3002000544108ULL,
-    0x4501100800148402ULL,
-    0x81440280100224ULL,
-    0x88008000000804ULL,
-    0x8084060000002812ULL,
-    0x1840201000108312ULL,
-    0x5080202000000141ULL,
-    0x1042a180880281ULL,
-    0x900802900c01040ULL,
-    0x8205104104120ULL,
-    0x9004220000440aULL,
-    0x8029510200708ULL,
-    0x8008440100404241ULL,
-    0x2420001111000bdULL,
-    0x4000882304000041ULL,
-};
-
-const Bitboard BMagic[64] = {
-    0x100420000431024ULL,
-    0x280800101073404ULL,
-    0x42000a00840802ULL,
-    0xca800c0410c2ULL,
-    0x81004290941c20ULL,
-    0x400200450020250ULL,
-    0x444a019204022084ULL,
-    0x88610802202109aULL,
-    0x11210a0800086008ULL,
-    0x400a08c08802801ULL,
-    0x1301a0500111c808ULL,
-    0x1280100480180404ULL,
-    0x720009020028445ULL,
-    0x91880a9000010a01ULL,
-    0x31200940150802b2ULL,
-    0x5119080c20000602ULL,
-    0x242400a002448023ULL,
-    0x4819006001200008ULL,
-    0x222c10400020090ULL,
-    0x302008420409004ULL,
-    0x504200070009045ULL,
-    0x210071240c02046ULL,
-    0x1182219000022611ULL,
-    0x400c50000005801ULL,
-    0x4004010000113100ULL,
-    0x2008121604819400ULL,
-    0xc4a4010000290101ULL,
-    0x404a000888004802ULL,
-    0x8820c004105010ULL,
-    0x28280100908300ULL,
-    0x4c013189c0320a80ULL,
-    0x42008080042080ULL,
-    0x90803000c080840ULL,
-    0x2180001028220ULL,
-    0x1084002a040036ULL,
-    0x212009200401ULL,
-    0x128110040c84a84ULL,
-    0x81488020022802ULL,
-    0x8c0014100181ULL,
-    0x2222013020082ULL,
-    0xa00100002382c03ULL,
-    0x1000280001005c02ULL,
-    0x84801010000114cULL,
-    0x480410048000084ULL,
-    0x21204420080020aULL,
-    0x2020010000424a10ULL,
-    0x240041021d500141ULL,
-    0x420844000280214ULL,
-    0x29084a280042108ULL,
-    0x84102a8080a20a49ULL,
-    0x104204908010212ULL,
-    0x40a20280081860c1ULL,
-    0x3044000200121004ULL,
-    0x1001008807081122ULL,
-    0x50066c000210811ULL,
-    0xe3001240f8a106ULL,
-    0x940c0204030020d4ULL,
-    0x619204000210826aULL,
-    0x2010438002b00a2ULL,
-    0x884042004005802ULL,
-    0xa90240000006404ULL,
-    0x500d082244010008ULL,
-    0x28190d00040014e0ULL,
-    0x825201600c082444ULL,
-};
+// Function declarations
+void initSlidingAttacks(void);
+Bitboard getRookAttacks(int square, Bitboard occupancy);
+Bitboard getBishopAttacks(int square, Bitboard occupancy);
+Bitboard getQueenAttacks(int square, Bitboard occupancy);
 
 // the above magic numbers were computed with the following code, courtesy of
 // https://www.chessprogramming.org/Looking_for_Magics
@@ -500,26 +335,6 @@ const Bitboard BMagic[64] = {
 //     return 0ULL;
 // }
 
-// int RBits[64] = {
-//     12, 11, 11, 11, 11, 11, 11, 12,
-//     11, 10, 10, 10, 10, 10, 10, 11,
-//     11, 10, 10, 10, 10, 10, 10, 11,
-//     11, 10, 10, 10, 10, 10, 10, 11,
-//     11, 10, 10, 10, 10, 10, 10, 11,
-//     11, 10, 10, 10, 10, 10, 10, 11,
-//     11, 10, 10, 10, 10, 10, 10, 11,
-//     12, 11, 11, 11, 11, 11, 11, 12};
-
-// int BBits[64] = {
-//     6, 5, 5, 5, 5, 5, 5, 6,
-//     5, 5, 5, 5, 5, 5, 5, 5,
-//     5, 5, 7, 7, 7, 7, 5, 5,
-//     5, 5, 7, 9, 9, 7, 5, 5,
-//     5, 5, 7, 9, 9, 7, 5, 5,
-//     5, 5, 7, 7, 7, 7, 5, 5,
-//     5, 5, 5, 5, 5, 5, 5, 5,
-//     6, 5, 5, 5, 5, 5, 5, 6};
-
 // int main()
 // {
 //     int square;
@@ -535,4 +350,51 @@ const Bitboard BMagic[64] = {
 //     printf("};\n\n");
 
 //     return 0;
+// }
+// Bit counts for indexing
+
+// the following functions were used to generate the occupancy maps
+// void computeRookOccupancyMaps()
+// {
+//     for (int square = 0; square < 64; square++)
+//     {
+//         int rank = square / 8;
+//         int file = square % 8;
+
+//         /* squares on same rank, excluding edge files A and H */
+//         Bitboard rankMask = RANKS[rank] & ~(FILE_A | FILE_H);
+
+//         /* squares on same file, excluding edge ranks 1 and 8 */
+//         Bitboard fileMask = FILES[file] & ~(RANK_1 | RANK_8);
+
+//         /* combine and remove the current square */
+//         Bitboard mask = (rankMask | fileMask) & ~square_mask(square);
+
+//         printf("0x%016llxULL,\n", mask);
+//     }
+// }
+// void computeBishopOccupancyMaps()
+// {
+//     printf("static const Bitboard bishop_occupancy_maps[] = {\n");
+//     for (int sq = 0; sq < 64; sq++)
+//     {
+//         int rank = sq / 8;
+//         int file = sq % 8;
+//         Bitboard mask = 0ULL;
+
+//         // NE
+//         for (int r = rank + 1, f = file + 1; r <= 6 && f <= 6; r++, f++)
+//             mask |= 1ULL << (r * 8 + f);
+//         // NW
+//         for (int r = rank + 1, f = file - 1; r <= 6 && f >= 1; r++, f--)
+//             mask |= 1ULL << (r * 8 + f);
+//         // SE
+//         for (int r = rank - 1, f = file + 1; r >= 1 && f <= 6; r--, f++)
+//             mask |= 1ULL << (r * 8 + f);
+//         // SW
+//         for (int r = rank - 1, f = file - 1; r >= 1 && f >= 1; r--, f--)
+//             mask |= 1ULL << (r * 8 + f);
+//         printf("    0x%016llxULL,%s", mask, (sq + 1) % 8 == 0 ? "\n" : " ");
+//     }
+//     printf("};\n");
 // }
