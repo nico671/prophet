b303ec276bd179d55898bf6b843439b88ecc05b3
Nico Carbone <carbone.nicolas0@gmail.com>
2026-03-11 18:16:25 +0100

    Refactor king and knight move generation: consolidate functions into constant_moves module and remove obsolete files

--- src/eval/hceval.h
+++ src/eval/hceval.h
@@ -7,9 +7,12 @@ typedef struct CBoard CBoard;
 #define BISHOP_VALUE 325
 #define ROOK_VALUE 500
 #define QUEEN_VALUE 900
-#define KING_VALUE 10000 // High value so the engine knows to protect it
+#define KING_VALUE 10000 // extremely high value so the engine knows to protect it
 
-// Initializes evaluation tables (safe to call multiple times).
+/**
+ * @brief Initializes the evaluation function, precomputing any necessary tables or parameters. This function is idempotent and can be safely called multiple times without adverse effects. It must be called before any calls to evaluateBoard to ensure that the evaluation function is properly initialized and ready for use.
+ *
+ */
 void hcEvalInit(void);
 
 /**
--- src/movegen/constant_moves.h
+++ src/movegen/constant_moves.h
@@ -1,9 +1,15 @@
-#ifndef PAWN_MOVES_H
-#define PAWN_MOVES_H
+#ifndef CONSTANT_MOVES_H
+#define CONSTANT_MOVES_H
 
 #include "movegen/move.h"
 
 typedef struct CBoard CBoard;
+
+void genAllPseudoLegalKingNonCastlingMoves(CBoard *board, MoveList *moveList);
+void genAllPseudoLegalKingCastlingMoves(CBoard *board, MoveList *moveList);
+void genAllPseudoLegalKingMoves(CBoard *board, MoveList *moveList);
+
+void genAllPseudoLegalKnightMoves(CBoard *board, MoveList *moveList);
 
 void genSinglePawnPushes(CBoard *board, MoveList *moveList);
 void genDoublePawnPushes(CBoard *board, MoveList *moveList);
@@ -11,5 +17,4 @@ void genPawnCaptures(CBoard *board, MoveList *moveList);
 void genPawnPromotions(CBoard *board, MoveList *moveList);
 void genEnPassantPawnMoves(CBoard *board, MoveList *moveList);
 void genAllPseudoLegalPawnMoves(CBoard *board, MoveList *moveList);
-
-#endif // PAWN_MOVES_H
+#endif // CONSTANT_MOVES_H
