a552f6c5811af3c11e485c547ed829141ef917a8
Nico Carbone <carbone.nicolas0@gmail.com>
2026-05-16 19:06:01 +0200

    chore: update ttbound doxygen

--- src/tt/tt.h
+++ src/tt/tt.h
@@ -4,20 +4,19 @@
 #include "board/cboard.h"
 #include "movegen/move.h"
 
+/**
+ * @brief Represents the type of a transposition table entry
+ *
+ * PV-nodes (Knuth's Type 1) are nodes that have a score that ends up being inside the window. So if the bounds passed are [a,b], with the score returned s, a<s<b. These nodes have all moves searched, and the value returned is exact (i.e., not a bound), which propagates up to the root along with a principal variation.
+ *
+ * Cut-nodes, known as fail-high nodes, are nodes in which a beta-cutoff was performed. So with bounds [a,b], s>=b. A minimum of one move at a Cut-node needs to be searched. The score returned is a lower bound (might be greater) on the exact score of the node
+ *
+ * All-nodes, known as fail-low nodes, are nodes in which no move's score exceeded alpha. With bounds [a,b], s<=a. Every move at an All-node is searched, and the score returned is an upper bound, the exact score might be less.
+ */
 typedef enum {
-    TT_PV, // PV-nodes (Knuth's Type 1) are nodes that have a score that ends up
-           // being inside the window. So if the bounds passed are [a,b], with
-           // the score returned s, a<s<b. These nodes have all moves searched,
-           // and the value returned is exact (i.e., not a bound), which
-           // propagates up to the root along with a principal variation.
-    TT_CUT, // known as fail-high nodes, are nodes in which a beta-cutoff was
-            // performed. So with bounds [a,b], s>=b. A minimum of one move at a
-            // Cut-node needs to be searched. The score returned is a lower bound
-            // (might be greater) on the exact score of the node
-    TT_ALL, // known as fail-low nodes, are nodes in which no move's score
-            // exceeded alpha. With bounds [a,b], s<=a. Every move at an All-node
-            // is searched, and the score returned is an upper bound, the exact
-            // score might be less.
+    TT_PV,
+    TT_CUT,
+    TT_ALL,
 } TTBound;
 
 typedef struct {
